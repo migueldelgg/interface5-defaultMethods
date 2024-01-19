010b2f8ed1829a4e16fbe5afeb2c5fa5933b0c15
4Miguel2 <delgdevoleper@gmail.com>
2024-01-19 20:30:54 -0300

    bad solution

---
+++ src/application/Program.java
@@ -0,0 +1,26 @@
+package application;
+
+import service.BrazilInterestService;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Program {
+    public static void main(String[] args) {
+
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Amount: ");
+        double amount = sc.nextDouble();
+        System.out.print("Months: ");
+        int months = sc.nextInt();
+
+        BrazilInterestService br = new BrazilInterestService(2.0);
+        double payment = br.payment(amount, months);
+
+        System.out.println("Payment after "+ months + " months:");
+        System.out.println(String.format("%.2f", payment));
+
+    }
+}
